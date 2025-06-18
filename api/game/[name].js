baae315a32c38479f5ac2d8b33d493fa8e934046
DoliScriptz <132125366+DoliScriptz@users.noreply.github.com>
2025-06-18 12:08:14 +0700

    Update [name].js

--- api/game/[name].js
+++ api/game/[name].js
@@ -3,14 +3,13 @@ import path from "path";
 
 export default async function handler(req, res) {
   const { name } = req.query;
-
   const ua = req.headers["user-agent"] || "";
-  if (!ua.includes("MakalHubExecutor")) return res.status(403).end("Forbidden");
+  if (!ua.includes("MakalHubExecutor")) return res.status(403).end();
 
-  const filePath = path.join(process.cwd(), "secure-scripts", `${name}.lua`);
-  if (!fs.existsSync(filePath)) return res.status(404).end("Script not found");
+  const file = path.join(process.cwd(), "secure-scripts", `${name}.lua`);
+  if (!fs.existsSync(file)) return res.status(404).end();
 
-  const content = fs.readFileSync(filePath, "utf8");
+  const code = fs.readFileSync(file, "utf8");
   res.setHeader("Content-Type", "text/plain");
-  res.status(200).send(content);
+  res.status(200).send(code);
 }
