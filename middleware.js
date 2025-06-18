29c271b8ddb71e3d5ded2ecd3a3c2fb7480d499b
DoliScriptz <132125366+DoliScriptz@users.noreply.github.com>
2025-06-18 12:51:36 +0700

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,14 +1,14 @@
 import { NextResponse } from 'next/server'
 
 export function middleware(req) {
-  const userAgent = req.headers.get("user-agent")
-  if (userAgent !== "MakalHubExecutor") {
-    return new NextResponse("Forbidden", { status: 403 })
-  }
-  return NextResponse.next()
-}
+  const ua = req.headers.get('user-agent')
+  const url = req.nextUrl
 
-// Apply to only /scripts/*
-export const config = {
-  matcher: ["/scripts/:path*"],
+  if (url.pathname.startsWith('/api/script/')) {
+    if (ua !== 'MakalHubExecutor') {
+      return new NextResponse('Forbidden', { status: 403 })
+    }
+  }
+
+  return NextResponse.next()
 }
